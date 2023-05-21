77a16c9f6c7714d3910d32dc24276a712c74b644
Ilyos Xolmamtov <ilyosxolmamatov3@gmail.com>
2023-05-21 14:49:46 +0500

    start project

---
+++ src/store/users/user.slice.js
@@ -0,0 +1,8 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const { action: UserAction, reducer: userReduser } = createSlice({
+    name: "user",
+    initialState: {
+        // user
+    }
+})
